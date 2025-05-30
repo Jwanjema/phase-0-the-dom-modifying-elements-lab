fdf2be65a6b0731373acbcc9f43e4eb7c32c9b3f
joewaweruwanjema <wawerujoe@gmail.com>
2025-05-30 13:55:38 +0300

    add index.html index.js package-lock.json

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+// Remove the <main> element with id 'main'
+const main = document.getElementById('main');
+if (main) {
+  main.remove();
+}
+
+// Create a new <h1> element and assign it to 'newHeader'
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Joe is the champion'; // <-- Replace YOUR-NAME with your actual name
+
+// Add the newHeader to the document body
+document.body.appendChild(newHeader);
