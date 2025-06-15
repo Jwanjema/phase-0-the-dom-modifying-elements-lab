e42c772aaa87a3332caa3501c7f48fc837adf6d6
joewaweruwanjema <wawerujoe@gmail.com>
2025-06-15 16:00:11 +0300

    add index.js

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-// Remove the <main> element with id 'main'
-const main = document.getElementById('main');
-if (main) {
-  main.remove();
+const mainElement = document.getElementById('main');
+if (mainElement){
+    mainElement.remove();
 }
 
-// Create a new <h1> element and assign it to 'newHeader'
-const newHeader = document.createElement('h1');
-newHeader.id = 'victory';
-newHeader.innerHTML = 'Joe is the champion'; // <-- Replace YOUR-NAME with your actual name
+const newHeader = document.createElement("h1");
 
-// Add the newHeader to the document body
-document.body.appendChild(newHeader);
+newHeader.id = 'victory'
+
+newHeader.textContent = "Joe is the champion";
+
